5209e873ac4c4f9902ce44bd4f252f569e22fac9
Akshay kumar Amavarapu <n200534@rguktn.ac.in>
2025-08-15 23:14:57 +0530

    fixed cors erros

--- Backend/server.js
+++ Backend/server.js
@@ -17,7 +17,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: true, // your frontend domain
+  origin: "*", // your frontend domain
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"]
